b9b3ae457b4860c71ad3c3c04dc785374ab3231c
Bodhitha Weerasekara <88415912+IT20204334@users.noreply.github.com>
2022-05-12 17:35:48 +0530

    update home

--- src/pages/customer/movieBook.jsx
+++ src/pages/customer/movieBook.jsx
@@ -1,123 +1,123 @@
-import React, { useState } from "react";
-import { Card, Col, Row, Modal, Form, Input, InputNumber, Select } from "antd";
-import "./movieBook.css";
-import Nav from "../../components/navbar";
+// import React, { useState } from "react";
+// import { Card, Col, Row, Modal, Form, Input, InputNumber, Select } from "antd";
+// import "./movieBook.css";
+// import Nav from "../../components/navbar";
 
-function MovieBook() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+// function MovieBook() {
+//   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
+//   const showModal = () => {
+//     setIsModalVisible(true);
+//   };
 
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
+//   const handleOk = () => {
+//     setIsModalVisible(false);
+//   };
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+//   const handleCancel = () => {
+//     setIsModalVisible(false);
+//   };
 
-  const [componentSize, setComponentSize] = useState("default");
+//   const [componentSize, setComponentSize] = useState("default");
 
-  const onFormLayoutChange = ({ size }) => {
-    setComponentSize(size);
-  };
+//   const onFormLayoutChange = ({ size }) => {
+//     setComponentSize(size);
+//   };
 
-  function onChange(value) {
-    console.log("changed", value);
-  }
+//   function onChange(value) {
+//     console.log("changed", value);
+//   }
 
-  function handleChange(value) {
-    console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
-  }
+//   function handleChange(value) {
+//     console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
+//   }
 
-  const { Option } = Select;
+//   const { Option } = Select;
 
-  return (
-    <>
-      <Nav />
+//   return (
+//     <>
+//       <Nav />
 
-      <div className="site-card-wrapper">
-        <div>
-          <Row gutter={16}>
-            <Col span={8}>
-              <Card
-                style={{ width: 250 }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://cdn.cbeditz.com/cbeditz/thumbnail/kgf-chapter-2-movie-poster-background-download-11610710099utufdj4uyl.webp"
-                  />
-                }
-              >
-                KGF Chapter 2<br />
-                Now Showing <br />
-                <button onClick={showModal}>
-                  <center>Add to cart</center>
-                </button>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </div>
-      <div>
-        <Modal
-          title="Book Your Ticket"
-          visible={isModalVisible}
-          onOk={handleOk}
-          onCancel={handleCancel}
-        >
-          <div>
-            {/* <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p> */}
+//       <div className="site-card-wrapper">
+//         <div>
+//           <Row gutter={16}>
+//             <Col span={8}>
+//               <Card
+//                 style={{ width: 250 }}
+//                 cover={
+//                   <img
+//                     alt="example"
+//                     src="https://cdn.cbeditz.com/cbeditz/thumbnail/kgf-chapter-2-movie-poster-background-download-11610710099utufdj4uyl.webp"
+//                   />
+//                 }
+//               >
+//                 KGF Chapter 2<br />
+//                 Now Showing <br />
+//                 <button onClick={showModal}>
+//                   <center>Add to cart</center>
+//                 </button>
+//               </Card>
+//             </Col>
+//           </Row>
+//         </div>
+//       </div>
+//       <div>
+//         <Modal
+//           title="Book Your Ticket"
+//           visible={isModalVisible}
+//           onOk={handleOk}
+//           onCancel={handleCancel}
+//         >
+//           <div>
+//             {/* <p>Some contents...</p>
+//         <p>Some contents...</p>
+//         <p>Some contents...</p> */}
 
-            <Form
-              labelCol={{
-                span: 4,
-              }}
-              wrapperCol={{
-                span: 14,
-              }}
-              layout="horizontal"
-              initialValues={{
-                size: componentSize,
-              }}
-              onValuesChange={onFormLayoutChange}
-              size={componentSize}
-            >
-              <Form.Item label="Email">
-                <Input />
-              </Form.Item>
+//             <Form
+//               labelCol={{
+//                 span: 4,
+//               }}
+//               wrapperCol={{
+//                 span: 14,
+//               }}
+//               layout="horizontal"
+//               initialValues={{
+//                 size: componentSize,
+//               }}
+//               onValuesChange={onFormLayoutChange}
+//               size={componentSize}
+//             >
+//               <Form.Item label="Email">
+//                 <Input />
+//               </Form.Item>
 
-              <Form.Item label="Ticket Qty">
-                <InputNumber
-                  min={1}
-                  max={10}
-                  defaultValue={3}
-                  onChange={onChange}
-                />
-              </Form.Item>
+//               <Form.Item label="Ticket Qty">
+//                 <InputNumber
+//                   min={1}
+//                   max={10}
+//                   defaultValue={3}
+//                   onChange={onChange}
+//                 />
+//               </Form.Item>
 
-              <Form.Item label="Time">
-                <Select
-                  labelInValue
-                  defaultValue={{ value: "lucy" }}
-                  style={{ width: 120 }}
-                  onChange={handleChange}
-                >
-                  <Option value="aam">10.30am</Option>
-                  <Option value="pm">1.30pm</Option>
-                  <Option value="am">10.30pm</Option>
-                </Select>
-              </Form.Item>
-            </Form>
-          </div>
-        </Modal>
-      </div>
-    </>
-  );
-}
+//               <Form.Item label="Time">
+//                 <Select
+//                   labelInValue
+//                   defaultValue={{ value: "lucy" }}
+//                   style={{ width: 120 }}
+//                   onChange={handleChange}
+//                 >
+//                   <Option value="aam">10.30am</Option>
+//                   <Option value="pm">1.30pm</Option>
+//                   <Option value="am">10.30pm</Option>
+//                 </Select>
+//               </Form.Item>
+//             </Form>
+//           </div>
+//         </Modal>
+//       </div>
+//     </>
+//   );
+// }
 
-export default MovieBook;
+// export default MovieBook;
