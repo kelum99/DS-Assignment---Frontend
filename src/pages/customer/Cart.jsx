bf3a9cf09a6d527376f2cee11a7b6d122f114775
vihaan1997 <vihaan1997@gmail.com>
2022-05-19 13:36:29 +0530

    Chanegs in Cart

--- src/pages/customer/Cart.jsx
+++ src/pages/customer/Cart.jsx
@@ -1,89 +1,194 @@
-import React from "react";
 import "antd/dist/antd.css";
-import "./Cart.css";
-import { Tag, Table, Space, Card } from "antd";
+import React from "react";
+import { Table, Modal, Input, Card } from "antd";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import {useNavigate} from "react-router-dom";
-import {EditOutlined, DeleteOutlined} from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import "./Cart.css";
 
-function Cart() {
-    const dataSource = [
+function Test(){
+    const [isEditing , setisEditing ] = useState(false);
+    const [editingCart, setEditingCart] = useState(null);
+    const [dataSource, setdataSource] = useState([
         {
-            key: '1',
-            id: 1,
-            email: 'vihanga@gmail.com',
-            mobileNumber: '0711234567',
-            theater: 'OGF',
+            id:1,
+            email:'john@gmail.com',
+            movie:'KGF',
+            theater:'Savoy',
             time: '10.30',
-            price: '2000',
-            action: 'action'
+            ticketQuantity: 2,
+            price: 1000,
         },
         {
-            key: '2',
-            id: 2,
-            email: 'nimal@gmail.com',
-            mobileNumber: '0712771079',
-            theater: 'Savoy',
+            id:2,
+            email:'john@gmail.com',
+            movie:'KGF',
+            theater:'Savoy',
             time: '10.30',
-            price: '2000',
-            action: 'action'
+            ticketQuantity: 2,
+            price: 1000,
         },
-    ];
-
-    const columns = [
         {
+            id:3,
+            email:'john@gmail.com',
+            movie:'KGF',
+            theater:'Savoy',
+            time: '10.30',
+            ticketQuantity: 2,
+            price: 1000,
+        },
+        {
+            id:4,
+            email:'john@gmail.com',
+            movie:'KGF',
+            theater:'Savoy',
+            time: '10.30',
+            ticketQuantity: 2,
+            price: 1000,
+        },
+    ]);
+
+    const columns=[
+        {
+            key: 1,
             title: 'ID',
             dataIndex: 'id',
-            key: 'id',
         },
         {
+            key: 2,
             title: 'Email',
             dataIndex: 'email',
-            key: 'email',
         },
         {
-            title: 'Mobile Number',
-            dataIndex: 'mobileNumber',
-            key: 'mobileNumber',
+            key: 3,
+            title: 'Movie',
+            dataIndex: 'movie',
         },
         {
+            key: 4,
             title: 'Theater',
             dataIndex: 'theater',
-            key: 'theater',
         },
         {
+            key: 5,
             title: 'Time',
             dataIndex: 'time',
-            key: 'time',
         },
         {
+            key: 6,
+            title: 'Ticket Quantity',
+            dataIndex: 'ticketQuantity',
+        },
+        {
+            key: 7,
             title: 'Price',
             dataIndex: 'price',
-            key: 'price',
         },
         {
+            key: 8,
             title: 'Actions',
-            key: 'action',
-            render:(record)=>{
-                return (
-                    <>
-                        <EditOutlined />
-                        <DeleteOutlined style={{ color:"red", marginLeft: 12}} />
-                    </>
-                );
+            render: (record) => {
+                return <>
+                    <EditOutlined
+                        onClick={()=>{
+                            onEditCart(record);
+                        }}
+                    />
+                        
+                    <DeleteOutlined 
+                        onClick={()=>{
+                            onDeleteCart(record);
+                        }} 
+                        style = {{ color: "red", marginLeft: 12  }} 
+                    />
+                </>
             }
         },
     ];
-    return (
+
+    const onEditCart = (record) => {
+        setisEditing(true);
+        setEditingCart({...record});
+    };
+
+    const onDeleteCart=(record)=>{
+        Modal.confirm({
+            title: 'Delete this cart item?',
+            okText: 'Yes',
+            okType: 'danger',
+            onOk:()=>{
+                setdataSource(pre=>{
+                    return pre.filter(cart => cart.id !== record.id);
+                });
+            }
+        });
+    };
+
+    const resetEditng=()=>{
+        setisEditing(false);
+        setEditingCart(null);
+    };
+
+    return(
         <div className="cart-main-component">
-            <Card 
+            <Card
                 className="cartCard"
                 title="Cart"
                 headStyle={{ fontSize: 30, fontWeight: "bold", border: "none" }}
             >
-                <Table dataSource={dataSource} columns={columns} />
+                <Table
+                    columns={columns}
+                    dataSource={dataSource}
+                >
+                </Table>
+                <Modal
+                    title= "Edit cart items"
+                    visible = {isEditing}
+                    okText="Save"
+                    onCancel={()=>{
+                        setisEditing(false);
+                        resetEditng();
+                    }}
+                    onOk={() => {
+                        setdataSource(pre=>{
+                            return pre.map(cart=>{
+                                if(cart.id === editingCart.id){
+                                    return editingCart;
+                                } else {
+                                    return cart;
+                                }
+                            });
+                        });
+                        resetEditng();
+                    }}
+                >
+                    <Input value={editingCart?.movie} onChange={(e)=>{
+                            setEditingCart(pre=>{
+                                return {...pre, movie:e.target.value}
+                            });
+                        }} 
+                    /> <br/>
+                    <Input value={editingCart?.theater} onChange={(e)=>{
+                            setEditingCart(pre=>{
+                                return {...pre, theater:e.target.value}
+                            });
+                        }} 
+                    /> <br/>
+                    <Input value={editingCart?.time} onChange={(e)=>{
+                            setEditingCart(pre=>{
+                                return {...pre, time:e.target.value}
+                            });
+                        }} 
+                    /> <br/>
+                    <Input value={editingCart?.ticketQuantity} onChange={(e)=>{
+                            setEditingCart(pre=>{
+                                return {...pre, ticketQuantity:e.target.value}
+                            });
+                        }} 
+                    />
+                </Modal>
             </Card>
         </div>
     );
 }
-export default Cart;
+export default Test;
